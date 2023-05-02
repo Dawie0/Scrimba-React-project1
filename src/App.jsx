837d13512433e1f6de6ab92ac83ccacb01fc32b7
Dawid Fourie <dawief4@gmail.com>
2023-05-01 21:11:05 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/navbar'
+import MainContent from './components/mainContent'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+    </div>
+  )
+}
+
+export default App
